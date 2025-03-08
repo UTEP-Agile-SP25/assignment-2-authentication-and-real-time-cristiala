339a812b480dd1ba40ac104c949e1bbc0c919e3e
Cristina Alarcon <cristina@alarcon.me>
2025-03-07 22:54:05 -0700

    Created setup

---
+++ src/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCoLj85FJGWGtEHD4bciZsif03K59J8arU",
+  authDomain: "alarcon-sandbox.firebaseapp.com",
+  projectId: "alarcon-sandbox",
+  storageBucket: "alarcon-sandbox.firebasestorage.app",
+  messagingSenderId: "929559237357",
+  appId: "1:929559237357:web:2badd9bc1ad5636434b2e4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
